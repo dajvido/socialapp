a27e7f17a9a364a425b819d33f3e6c353bb89e1b
dajvido <dawid.jaskot@gmail.com>
2015-05-09 14:13:55 +0200

    Make all Messages Controller tests pass

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,8 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @messages = Message.order('created_at DESC')
   end
 
   def new
@@ -7,8 +10,24 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params.merge({ user: current_user }))
+
+    if @message.save
+      redirect_to messages_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
 end
