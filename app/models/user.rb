7f2a73e524beb4f22cfa30162412930adf107c1e
dajvido <dawid.jaskot@gmail.com>
2015-05-09 12:18:16 +0200

    Add Message model

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@
 class User < ActiveRecord::Base
   attr_accessor :virtual_login
 
+  has_many :messages
+
   has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
